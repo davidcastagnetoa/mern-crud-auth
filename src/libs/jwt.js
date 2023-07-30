44ff2b9d206d2fa9cd5b125f06ac17aa8fdd3620
David Castagneto A <davidcastagnetoa@gmail.com>
2023-07-30 17:09:25 +0200

    env

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,13 +1,16 @@
 // el token es la crendencial o el string que devuevle el frontend confirma que el usuario se ha logueado como un pase o ticket
 // se crea el token con el id del usuario
-import { TOKEN_SECRET } from "../config.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 import jwt from "jsonwebtoken";
 
 export function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
-      TOKEN_SECRET,
+      process.env.TOKEN_SECRET,
       {
         expiresIn: "1d",
       },
