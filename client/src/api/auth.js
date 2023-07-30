284d4349280c2400e676c20d0a9c52bacb49c5f2
David Castagneto A <davidcastagnetoa@gmail.com>
2023-07-30 18:02:04 +0200

    node version

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,8 +1,6 @@
 import axios from "./axios";
 // import axios from "axios";
 
-// const api = "http://localhost:3000/api";
-
 export const registerRequest = async (user) => {
   // const response = await axios.post(`${api}/register`, user);
   const response = await axios.post(`/register`, user);
--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://tasks-server-mern.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
