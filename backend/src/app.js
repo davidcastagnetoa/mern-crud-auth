debd86c5903c4f778ef1148228f19ffefcd6b5e3
David Castagneto A <davidcastagnetoa@gmail.com>
2023-07-30 18:25:47 +0200

    node backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+import { FRONTEND_URL } from "./config.js";
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
     credentials: true,
   })
 );
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,29 +1,9 @@
 import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//     });
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// }
-
-// export default connectDB;
+import { MONGODB_URI } from "./config.js";
 
 export const connectDB = async () => {
   try {
-    // localhost or 127.0.0.1
-    // await mongoose.connect("mongodb://127.0.0.1/merndb");
-    // Mongo DB
-    await mongoose.connect(
-      "mongodb+srv://davidcastagneto:Nefthis1989@mern-users.nwszua8.mongodb.net/"
-    );
+    await mongoose.connect(MONGODB_URI);
     console.log("MongoDB Connected...");
   } catch (error) {
     console.log(error);
