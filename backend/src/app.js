0d2eb443ba74afd7333f5db9ae868d6bdc425a4e
David Castagneto A <davidcastagnetoa@gmail.com>
2023-08-05 00:49:45 +0200

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,20 +7,31 @@ import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://mern-crud-auth.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
+const whitelist = [
+  "https://mern-crud-auth.vercel.app",
+  "http://localhost:5173",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+};
+
+app.use(cors(corsOptions));
+
+// app.use(
+//   cors({
+//     origin: "https://mern-crud-auth.vercel.app",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   })
+// );
 
 // app.use(cors({ credentials: true }));
 
