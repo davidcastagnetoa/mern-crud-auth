cfa8a66d054f0ad0b845770621c5cb179564a5b8
David Castagneto A <davidcastagnetoa@gmail.com>
2023-08-05 00:25:37 +0200

    test token

--- backend/src/libs/jwt.js
+++ backend/src/libs/jwt.js
@@ -6,8 +6,26 @@ dotenv.config();
 
 import jwt from "jsonwebtoken";
 
+// export function createAccessToken(payload) {
+//   console.log(process.env.TOKEN_SECRET);
+//   return new Promise((resolve, reject) => {
+//     jwt.sign(
+//       payload,
+//       process.env.TOKEN_SECRET,
+//       {
+//         expiresIn: "1d",
+//       },
+//       (error, token) => {
+//         if (error) reject(error);
+//         resolve(token);
+//       }
+//     );
+//   });
+// }
+
 export function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
+    console.log(process.env.TOKEN_SECRET);
     jwt.sign(
       payload,
       process.env.TOKEN_SECRET,
@@ -15,7 +33,10 @@ export function createAccessToken(payload) {
         expiresIn: "1d",
       },
       (error, token) => {
-        if (error) reject(error);
+        if (error) {
+          console.log("Error signing token: ", error);
+          reject(error);
+        }
         resolve(token);
       }
     );
