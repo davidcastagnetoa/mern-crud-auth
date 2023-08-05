8f0191af3ab73533096dd07efe858e43bca9c026
David Castagneto A <davidcastagnetoa@gmail.com>
2023-08-06 00:57:02 +0200

    client free cors, added AWS

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL:
+    "http://ec2-44-204-81-214.compute-1.amazonaws.com:3000/api",
   // baseURL: "https://tasks-server-mern.onrender.com/api",
   withCredentials: true,
 });
