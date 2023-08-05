4401f057b7c2b29995bc2b242767212cd09ba737
David Castagneto A <davidcastagnetoa@gmail.com>
2023-08-05 19:26:30 +0200

    token

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "Some Secret Key";
 
 export const FRONTEND_URL =
   process.env.FRONTEND_URL || "http://localhost:5173";
