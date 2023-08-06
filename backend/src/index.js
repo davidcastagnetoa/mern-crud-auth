cf4b7825077b5693642be993a668421700aaed62
David Castagneto A <davidcastagnetoa@gmail.com>
2023-08-06 02:29:58 +0200

    port 80 app.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
   res.send("Greetings, Chief Master");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(80, () => {
+  console.log("Server is running on port 80");
 });
