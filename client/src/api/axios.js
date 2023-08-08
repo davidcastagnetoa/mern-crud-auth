9620f21eb8b0d9d03689c0b156a37cfe7e5d130e
David Castagneto Aguirre <david.castagneto@securitasdirect.es>
2023-08-08 15:39:47 +0200

    render client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:3000/api",
   // baseURL: "http://ec2-3-95-237-242.compute-1.amazonaws.com/api",
-  // baseURL: "https://tasks-server-mern.onrender.com/api",
-  baseURL: "https://mern-crud-auth-backend.vercel.app/api",
+  baseURL: "https://tasks-server-mern.onrender.com/api",
+  // baseURL: "https://mern-crud-auth-backend.vercel.app/api",
   withCredentials: true,
 });
 
